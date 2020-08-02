7dff766f3e7a34a592ba61b023ffadf25a419a26
zce <w@zce.me>
2020-08-02 22:23:28 +0800

    feat: initial commit

--- template/bin/{name}.js
+++ template/bin/{name}.js
@@ -4,14 +4,26 @@ const cac = require('cac')
 const <%= _.camelCase(name) %> = require('..')
 const { name, version } = require('../package')
 
+// Unified error handling
+/* istanbul ignore next */
+const onError = err => {
+  console.error(err.message)
+  process.exit(1)
+}
+
+process.on('uncaughtException', onError)
+process.on('unhandledRejection', onError)
+
 const cli = cac(name)
 
 // TODO: Implement module cli
 
-cli.option('--host <host>', 'Email host')
+cli
+  .command('<input>', 'Sample cli program')
+  .option('--host <host>', 'Sample options')
+  .example(`  $ ${name} w --host zce.me`)
+  .action((input, options) => {
+    console.log(<%= _.camelCase(name) %>(input, options))
+  })
 
-cli.help().version(version)
-
-const { args: [input], options: { host } } = cli.parse()
-
-console.log(<%= _.camelCase(name) %>(input, { host }))
+cli.help().version(version).parse()
